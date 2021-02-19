2436e49165e8eab23ab87ae1fc49a831365eafe3
Dmitriy <yakimenkodmitriy0@gmail.com>
2021-02-19 20:57:09 +0200

    qwe

---
+++ assets/js/massive.js
@@ -0,0 +1,23 @@
+/*let massive = ['alla', 'olla', 'Kolya'];
+
+massive.push('Vasya');
+massive.unshift('Katya');
+for (let item of massive) {
+	console.log(`Hello ${item}`);
+}
+
+const limit = 6000;
+let addPay = 0;
+let mas = [7000, 9800, 2500, 7000, 5000, 3500, 19000, 1200, 12000];
+
+for (let item of mas) {
+
+	if (item < limit) {
+		let diff = limit - item;
+		addPay = addPay + diff;
+	}
+
+
+}
+console.log(addPay);
+*/

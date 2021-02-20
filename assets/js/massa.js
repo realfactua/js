35076391ba60b2867c3e00262a226078e2aab045
Dmitriy <yakimenkodmitriy0@gmail.com>
2021-02-20 10:58:19 +0200

    йцу

--- assets/js/massa.js
+++ assets/js/massa.js
@@ -1,34 +1,31 @@
-let massa = +prompt(`Введите ваш вес в кг!`);
+// Индекс масси тела //  моя первая функция JavaScript:%) И оно работает:)
+function indexMassBody() {
+	let mass = +floatingInputMass.value;		 // переменнЫе  со значением 
+	let rost = +floatingInputRost.value;
 
-console.log(`Ваш вес ${massa}`);
+	let index = mass / (rost * rost) * 10000;	 // формула для расчета Индекса 
 
-let height = +prompt(`Ведите ваш рост в см`);
+	index = Math.round(index); 					 // функция округления до целого числа
 
+	resultIndex.innerHTML = `<h3 class=" m-1 pb-1 p-1 bg-primary text-white">индекс: <big><b>${index}</h3>`; 			   	// вывод в разметку результата расчета формулы 
 
-console.log(`Ваш рост ${height}`);
-let index = massa / ((height * height) / 100);
+	let answer = index;  						// присваиваем значение новой переменной 
 
+	if (answer <= 16) { 											// если 
+		result.innerHTML = 'Выраженный дефицит массы тела';			// выводим в разметку
+	} else if ((answer >= 16.1) && (answer <= 24)) {
+		result.innerHTML = 'Нормальная масса тела';
+	} else if ((answer >= 24) && (answer <= 30)) {					//	или если  
+		result.innerHTML = 'Избыточная масса тела (предожирение)';	// выводим в разметку
+	} else if ((answer >= 30) && (answer <= 35)) {
+		result.innerHTML = 'Ожирение I степени';
+	} else if ((answer >= 35) && (answer <= 40)) {
+		result.innerHTML = 'Ожирение II степени';
+	} else if ((answer >= 40) && (answer <= 50)) {
+		result.innerHTML = 'Ожирение III степени';
+	} else {																				// если не чего не подошло  
+		result.innerHTML = 'с такими пропорциями долго не протянуть...Покажитесь доктору!'; // выводим в разметку
+	}
+}
 
-console.log(`Ваш индекс тела ${index}`);
-if (index <= 16) {
-	console.log(`Выраженный дефицит массы тела`);
-}
-if ((index >= 16) && (index < 18.5)) {
-	console.log(`Недостаточная масса тела (дефицит)`);
-}
-if ((index >= 18.5) && (index < 24)) {
-	console.log(`Нормальная масса тела`);
-}
-if ((index >= 24) && (index < 30)) {
-	console.log(`Избыточная масса тела (предожирение)`);
-}
-if ((index >= 30) && (index < 35)) {
-	console.log(`Ожирение I степени`);
-}
-if ((index >= 35) && (index < 40)) {
-	console.log(`Ожирение II степени`);
-}
-if (index > 40) {
-	console.log(`Ожирение III степени`);
-}
 
